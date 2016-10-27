c1033e3c34b769888d1d4398645f39ff9d265c0a
HeewonChae <korman2444@gmail.com>
2016-10-28 05:04:30 +0900

    사용자로부터 url요청이 잇을경우 처리 포트번호 80으로 바꿈

--- src/main/java/webserver/RequestHandler.java
+++ src/main/java/webserver/RequestHandler.java
@@ -1,13 +1,19 @@
 package webserver;
 
+import java.io.BufferedReader;
 import java.io.DataOutputStream;
+import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.net.Socket;
+import java.nio.file.Files;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import util.Utility;
 
 public class RequestHandler extends Thread {
     private static final Logger log = LoggerFactory.getLogger(RequestHandler.class);
@@ -24,15 +30,38 @@ public class RequestHandler extends Thread {
 
         try (InputStream in = connection.getInputStream(); OutputStream out = connection.getOutputStream()) {
             // TODO 사용자 요청에 대한 처리는 이 곳에 구현하면 된다.
+        	
+        	//사용자로부터 입력값을 받아오기위해..
+        	BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(in));
+        	String line;
+        	String url;
+        	byte[] body;
+        	
+        	do{
+        		line = bufferedReader.readLine();
+        		if(line == null) return;
+        		
+        		if((url = Utility.parseURI(line)) != null){
+        			log.debug("요청한 url = " + url);
+        			body = Files.readAllBytes(new File("./webapp" + url).toPath());
+        			break;
+        		}
+        		else{
+        			  body = "Hello World".getBytes();
+        			  break;
+        		}
+        		
+        	}while(!"".equals(line));
+        	
             DataOutputStream dos = new DataOutputStream(out);
-            byte[] body = "Hello World".getBytes();
             response200Header(dos, body.length);
             responseBody(dos, body);
         } catch (IOException e) {
             log.error(e.getMessage());
         }
     }
-
+    
+    //헤더를 만드는 부분
     private void response200Header(DataOutputStream dos, int lengthOfBodyContent) {
         try {
             dos.writeBytes("HTTP/1.1 200 OK \r\n");
