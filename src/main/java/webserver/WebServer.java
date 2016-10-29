3f2308a61b464dcac86bdd8ac5e18eb784668f37
HeewonChae <korman2444@gmail.com>
2016-10-29 14:40:54 +0900

    커밋함 내용바뀐거 없음

--- src/main/java/webserver/WebServer.java
+++ src/main/java/webserver/WebServer.java
@@ -9,7 +9,7 @@ import org.slf4j.LoggerFactory;
 public class WebServer {
     private static final Logger log = LoggerFactory.getLogger(WebServer.class);
     private static final int DEFAULT_PORT = 80;
-
+    
     public static void main(String args[]) throws Exception {
         int port = 0;
         if (args == null || args.length == 0) {
@@ -26,7 +26,7 @@ public class WebServer {
             // 클라이언트가 연결될때까지 대기한다.
             Socket connection;
             while ((connection = listenSocket.accept()) != null) {
-                RequestHandler requestHandler = new RequestHandler(connection);
+                Thread requestHandler = new RequestHandler(connection);
                 requestHandler.start();
             }
         }
